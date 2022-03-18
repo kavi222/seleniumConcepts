a88ab857b3c9e4a591af1b72958c00de52acd1d9
LENOVO <LENOVO@10.50.53.18>
2022-03-18 14:36:02 +0530

    Committing Iframe concepts

---
+++ src/main/java/frames/InnerFrame2.java
@@ -0,0 +1,26 @@
+package frames;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class InnerFrame2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.w3schools.com/html/tryit.asp?filename=tryhtml_iframe_height_width");
+		driver.manage().window().maximize();
+		driver.switchTo().frame("iframeResult"); //switch into outer iframe
+		driver.switchTo().frame(0);
+		System.out.println("Text present in inner iframe : "+driver.findElement(By.xpath("//h1")).getText());
+		driver.switchTo().parentFrame();// go to parent frame/ outer frame
+		System.out.println("Text present in outer iframe : "+driver.findElement(By.xpath("//body/p")).getText());
+		
+	}
+
+}
